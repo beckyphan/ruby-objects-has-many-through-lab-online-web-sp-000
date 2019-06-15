798c7443ac9124d09b485d4d281f1cee0914a05f
Becky Phan <beckyphan@live.com>
2019-06-15 16:02:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   end
 
   def new_appointment(patient:, date:)
-    Appointment.new(date:, patient:, self)
+    Appointment.new(date:, patient:, doctor: self)
   end
 
   def appointments
